1de7ccd878067a2b16b4c351059ac3c72ac2e360
Yaroslav Korotaev <yaroslav.korotaev@gmail.com>
2023-08-11 00:16:44 +0300

    feat: add resources to functions

--- src/func.ts
+++ src/func.ts
@@ -1,38 +1,55 @@
 import type {
   SchemaIs,
+  BlueprintRef,
   BlockSpec,
 } from 'triex-types';
 import { block } from './block';
 
-export type FuncExecCallback<A, R> = (args: A) => Promise<R | R[]>;
-
-export type FuncArgsBuilder<A, R> = {
-  (): FuncBuilder<A, R>;
-  <T>(is: SchemaIs<T>): FuncBuilder<T, R>;
+export type FuncResourceBuilder<RS, A, R> = {
+  one<T>(ref: BlueprintRef<T>): FuncBuilder<T, A, R>;
 };
 
-export type FuncResultBuilder<A, R> = {
-  (): FuncBuilder<A, R>;
-  <T>(is: SchemaIs<T>): FuncBuilder<A, T>;
+export type FuncExecContext<RS, A> = {
+  resource: RS;
+  args: A;
 };
 
-export type FuncExecBuilder<A, R> = {
-  (callback: FuncExecCallback<A, R>): FuncBuilder<A, R>;
+export type FuncExecCallback<RS, A, R> = (ctx: FuncExecContext<RS, A>) => Promise<R | R[]>;
+
+export type FuncArgsBuilder<RS, A, R> = {
+  (): FuncBuilder<RS, A, R>;
+  <T>(is: SchemaIs<T>): FuncBuilder<RS, T, R>;
 };
 
-export type FuncBuilder<A, R> = {
-  args: FuncArgsBuilder<A, R>;
-  result: FuncResultBuilder<A, R>;
-  exec: FuncExecBuilder<A, R>;
+export type FuncResultBuilder<RS, A, R> = {
+  (): FuncBuilder<RS, A, R>;
+  <T>(is: SchemaIs<T>): FuncBuilder<RS, A, T>;
+};
+
+export type FuncExecBuilder<RS, A, R> = {
+  (callback: FuncExecCallback<RS, A, R>): FuncBuilder<RS, A, R>;
+};
+
+export type FuncBuilder<RS, A, R> = {
+  resource: FuncResourceBuilder<RS, A, R>;
+  args: FuncArgsBuilder<RS, A, R>;
+  result: FuncResultBuilder<RS, A, R>;
+  exec: FuncExecBuilder<RS, A, R>;
   spec(): BlockSpec;
 };
 
-export function func(): FuncBuilder<void, void> {
+export function func(): FuncBuilder<void, void, void> {
   let blockBuilder = block()
     .params()
   ;
   
-  const builder: FuncBuilder<any, any> = {
+  const builder: FuncBuilder<any, any, any> = {
+    resource: {
+      one: ref => {
+        blockBuilder.resource.one(ref);
+        return builder;
+      },
+    },
     args: (is?: SchemaIs<object>) => {
       if (is) {
         blockBuilder.input.one(is);
@@ -51,7 +68,10 @@ export function func(): FuncBuilder<void, void> {
     },
     exec: callback => {
       blockBuilder.process(async ctx => {
-        const result = await callback(ctx.params);
+        const result = await callback({
+          resource: ctx.resource,
+          args: ctx.params,
+        });
         
         if (Array.isArray(result)) {
           ctx.output.push(result);
